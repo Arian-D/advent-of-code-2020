afdf7f5899c47bac2351735e4685628ce332c7bd
Arian-D <ArianDehghani@protonmail.com>
2020-12-02 19:59:40 -0800

    Did the first day in Python

---
+++ day-1/python/day-1.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def product_that_sums_up_to_2020(entries: List[int]) -> int:
+    as_set = set(entries)
+    for entry in entries:
+        if 2020 - entry in as_set:
+            return entry * (2020 - entry)
+    raise ValueError("Not found")
+
+def main() -> None:
+    try:
+        print(
+            product_that_sums_up_to_2020(
+                [int(n) for n in open("input.txt").read().split()]
+            )
+        )
+    except:
+        print("Rip")
+
+if __name__ == "__main__":
+    main()
